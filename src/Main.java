20c6033360e8de11d42bb002a86e36b7c3a466c4
sonic-nick <skypotatoe22@gmail.com>
2020-01-29 14:03:26 -0500

    hi

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    private static int[] backPack;
+
+    public Main(){
+            backPack = new int[100];
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
